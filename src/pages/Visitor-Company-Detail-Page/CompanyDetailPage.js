552ecadca7c5f5df76ef9004848195dd42af0a84
Tunç Cenk Ergüven <erguventunccenk@gmail.com>
2023-06-11 00:08:01 +0300

    CompanyDetailPage Completed

--- src/pages/Visitor-Company-Detail-Page/CompanyDetailPage.js
+++ src/pages/Visitor-Company-Detail-Page/CompanyDetailPage.js
@@ -2,12 +2,14 @@ import React from 'react'
 import withRoot from './withRoot';
 import AppAppBar from './views/AppAppBar';
 import CompanyContent from './views/CompanyContent';
+import AppFooter from './views/AppFooter';
 
 function CompanyDetailPage() {
     return (
         <>
             <AppAppBar />
             <CompanyContent />
+            <AppFooter />
         </>
     )
 }
--- src/pages/Visitor-Company-Detail-Page/views/AppAppBar.js
+++ src/pages/Visitor-Company-Detail-Page/views/AppAppBar.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import axios from 'axios';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import Avatar from '@mui/material/Avatar';
 import AppBar from '../components/AppBar';
 import Toolbar from '../components/Toolbar';
+
 
 const rightLink = {
   fontSize: 16,
@@ -11,7 +13,35 @@ const rightLink = {
   ml: 3,
 };
 
+
+
+
 function AppAppBar() {
+
+  const [profile, setProfile] = React.useState({
+    avatar: '',
+  })
+  const token = localStorage.getItem('token');
+
+
+  React.useEffect(() => {
+    if (token != null) {
+      axios.get(`http://localhost:9080/api/v1/user-profile/get-profile-avatar/${token}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(response => {
+          const data = response.data;
+          setProfile(data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
+  }, [])
+
+
   return (
     <div>
       <AppBar position="fixed">
@@ -21,13 +51,16 @@ function AppAppBar() {
             variant="h6"
             underline="none"
             color="inherit"
-            href="/premium-themes/onepirate/"
+            href="/visitor"
             sx={{ fontSize: 24 }}
           >
             {'HR SOLUTIONS'}
           </Link>
           <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
-            <Avatar sx={{ bgcolor: "#ffa726" }}>H</Avatar>
+            <Avatar sx={{ bgcolor: "#ffa726" }}
+              src={profile === undefined
+                ? undefined
+                : "profile.avatar"} />
           </Box>
         </Toolbar>
       </AppBar>
