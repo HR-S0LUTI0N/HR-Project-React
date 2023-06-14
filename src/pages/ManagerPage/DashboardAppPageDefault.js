a1d7f92a9b7decad8d9d885b1725e58921fcbad7
Tunç Cenk Ergüven <erguventunccenk@gmail.com>
2023-06-14 22:55:54 +0300

    completed

--- src/pages/ManagerPage/DashboardAppPageDefault.js
+++ src/pages/ManagerPage/DashboardAppPageDefault.js
@@ -206,8 +206,6 @@ export default function DashboardAppPage() {
               }))}
             />
           </Grid>
-
-
         </Grid>
       </Container>
     </>
--- src/sections/@dashboard/app/AppConversionRates.js
+++ src/sections/@dashboard/app/AppConversionRates.js
@@ -1,29 +1,18 @@
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import dayjs from 'dayjs';
-
-import ReactApexChart from 'react-apexcharts';
+import React, { useState } from "react";
+import Autocomplete from '@mui/material/Autocomplete';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import TextField from '@mui/material/TextField';
+import { Box, Card, CardHeader } from '@mui/material';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import TextField from '@mui/material/TextField';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-import React, { useState } from "react";
-import Autocomplete from '@mui/material/Autocomplete';
-
-
-// @mui
-import { Box, Card, CardHeader } from '@mui/material';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-// utils
-import { fNumber } from '../../../utils/formatNumber';
-// components
-import { useChart } from '../../../components/chart';
-
-// ----------------------------------------------------------------------
 
 AppConversionRates.propTypes = {
   title: PropTypes.string,
@@ -31,121 +20,129 @@ AppConversionRates.propTypes = {
   chartData: PropTypes.array.isRequired,
 };
 
-const comboOptions = ['MALE', 'FEMALE', 'OTHER'];
-
-
 export default function AppConversionRates({ title, subheader, chartData, ...other }) {
+  const comboOptions = ['MALE', 'FEMALE', 'OTHER'];
+
   const [iseInformationBoxShown, setIsInformationBoxShown] = useState(true);
   const buttonHandler = () => {
     setIsInformationBoxShown(!iseInformationBoxShown);
   };
-  const [iseAddressBoxShown, setIsAddressBoxShown] = useState(true);
-  const buttonHandlerAddress = () => {
-    setIsAddressBoxShown(!iseAddressBoxShown);
-  };
-
-  const [value, setValue] = React.useState(dayjs('2022-04-17'));
-  const [comboValue, setComboValue] = React.useState(comboOptions[0]);
-  const [inputValue, setInputValue] = React.useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    {
-      const formData = {
-        email: event.target.email.value,
-        name: event.target.name.value,
-        surname: event.target.surname.value,
-        password: event.target.password.value,
-        identificationNumber: event.target.identificationNumber.value,
-        department: event.target.department.value,
-        dateOfBirth: event.target.dateOfBirth.value,
-      };
-      try {
-        console.log(formData);
-        const response = await axios.post('http://localhost:9090/api/v1/auth/register-manager', formData);
-        console.log('Success:', response.data);
-      } catch (error) {
-        console.error('Error create:', error);
-      }
+    const data = new FormData(event.currentTarget);
+    const token = localStorage.getItem('token');
+
+    const payload = {
+      password: data.get('password'),
+      email: data.get('email'),
+      birthPlace: data.get('birthPlace'),
+      name: data.get('name'),
+      middleName: data.get('middleName'),
+      surname: data.get('surname'),
+      dateOfBirth: data.get('dateOfBirth'),
+      identificationNumber: data.get('identificationNumber'),
+      gender: data.get('gender'),
+      phone: data.get('phone'),
+      wage: data.get('wage'),
+      wageDate: data.get('wageDate'),
+      avatar: data.get('avatar'),
+      neighbourhood: data.get('neighbourhood'),
+      district: data.get('district'),
+      province: data.get('province'),
+      country: data.get('country'),
+      buildingNumber: data.get('buildingNumber'),
+      apartmentNumber: data.get('apartmentNumber'),
+      postalCode: data.get('postalCode'),
+      department: data.get('department'),
+      jobStartingDate: data.get('jobStartingDate'),
+    };
+    console.log('Form Data:', payload);
+
+    try {
+      const response = await axios.post(`http://localhost:9080/api/v1/user-profile/create-personal/${token}`, payload);
+      console.log('Success:', response.data);
+    } catch (error) {
+      console.error('Error:', error);
     }
   };
 
   return (
-
     <Card {...other}>
-
       <CardHeader title={title} subheader={subheader} />
-      <Grid container sx={{ mx: 98, width: 200 }} >
-        <Button sx={{ marginBottom: '2rem' }} endIcon={iseInformationBoxShown ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />} variant="contained" onClick={buttonHandler} >
+      <Grid container sx={{ mx: 98, width: 200 }}>
+        <Button sx={{ marginBottom: '2rem' }} endIcon={iseInformationBoxShown ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />} variant="contained" onClick={buttonHandler}>
           {iseInformationBoxShown ? "Information " : "Information"}
         </Button>
       </Grid>
-      {iseInformationBoxShown && <Card> <Box onSubmit={handleSubmit}
-        component="form"
-        sx={{
-          '& > :not(style)': { m: 1, width: '30ch' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
 
-        <TextField id="name" name="name" label="Name" variant="filled" />
-        <TextField id="middleName" name="middleName" label="Middle Name" variant="filled" />
-        <TextField id="surname" name="surname" label="Surname" variant="filled" />
-        <TextField id="email" name="email" label="Email" variant="filled" />
-        <TextField id="password" name="password" label="Password" variant="filled" />
-        <TextField id="birthPlace" name="birthPlace" label="Birth Place" variant="filled" />
-        <TextField id="identificationNumber" name="identificationNumber" label="Identification Number" variant="filled" />
-        <TextField id="phone" name="phone" label="Phone" variant="filled" />
-        <TextField id="wage" name="wage" label="Wage" variant="filled" />
-        <TextField id="wageDate" name="wageDate" label="Wage Date" variant="filled" />
-        <TextField id="department" name="department" label="Department" variant="filled" />
-        <TextField id="jobStartingDate" name="jobStartingDate" label="Job Starting Date" variant="filled" />
-        <div style={{ display: 'flex' }}>
-          <div style={{ marginRight: '1rem' }}>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DemoContainer components={['DatePicker', 'DatePicker']}>
-                <DatePicker label="Date Of Birth" defaultValue={dayjs('2022-04-17')} sx={{ width: 294 }} />
-              </DemoContainer>
-            </LocalizationProvider>
-          </div>
-          <div>
-            <Autocomplete
-              value={comboValue}
-              onChange={(event, newValue) => {
-                setComboValue(newValue);
-              }}
-              inputValue={inputValue}
-              onInputChange={(event, newInputValue) => {
-                setInputValue(newInputValue);
-              }}
-              id="controllable-states-demo"
-              options={comboOptions}
-              sx={{ width: 294, marginTop: 0.94 }}
-              renderInput={(params) => <TextField {...params} label="Gender" />}
-            />
-          </div>
-        </div>
-        <CardHeader subheader="Employee Address" />
+      {iseInformationBoxShown && (
+        <Card>
+          <Box
+            component="form"
+            sx={{
+              '& > :not(style)': { m: 1, width: '30ch' },
+            }}
+            noValidate
+            autoComplete="off"
+            onSubmit={handleSubmit}
+          >
+            <TextField id="name" name="name" label="Name" variant="filled" />
+            <TextField id="middleName" name="middleName" label="Middle Name" variant="filled" />
+            <TextField id="surname" name="surname" label="Surname" variant="filled" />
+            <TextField id="email" name="email" label="Email" variant="filled" />
+            <TextField id="password" name="password" label="Password" variant="filled" />
+            <TextField id="birthPlace" name="birthPlace" label="Birth Place" variant="filled" />
+            <TextField id="identificationNumber" name="identificationNumber" label="Identification Number" variant="filled" />
+            <TextField id="phone" name="phone" label="Phone" variant="filled" />
+            <TextField id="wage" name="wage" label="Wage" variant="filled" />
+            <TextField id="wageDate" name="wageDate" label="Wage Date" variant="filled" />
+            <TextField id="department" name="department" label="Department" variant="filled" />
+            <TextField id="jobStartingDate" name="jobStartingDate" label="Job Starting Date" variant="filled" />
 
-        <TextField id="neighbourhood" name="neighbourhood" label="Neighbourhood" variant="filled" />
-        <TextField id="district" name="district" label="District" variant="filled" />
-        <TextField id="province" name="province" label="Province" variant="filled" />
-        <TextField id="country" name="country" label="Country" variant="filled" />
-        <TextField id="buildingNumber" name="buildingNumber" label="Building Number" variant="filled" />
-        <TextField id="apartmentNumber" name="apartmentNumber" label="Apartment Number" variant="filled" />
-        <TextField id="postalCode" name="postalCode" label="Postal Code" variant="filled" />
+            <div style={{ display: 'flex' }}>
+              <div style={{ marginRight: '1rem' }}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DemoContainer components={['DatePicker', 'DatePicker']}>
+                    <DatePicker
+                      label="Date Of Birth"
+                      name="dateOfBirth"
+                      value={dayjs('2022-04-17')}
+                      onChange={() => { }} // Add your logic here
+                      sx={{ width: 294 }}
+                    />
+                  </DemoContainer>
+                </LocalizationProvider>
+              </div>
+              <div>
+                <Autocomplete
+                  sx={{ width: 294, mt: 1 }}
+                  name="gender"
+                  options={comboOptions}
+                  defaultValue={comboOptions[0]}
+                  renderInput={(params) => <TextField {...params} label="Gender" />}
+                />
+              </div>
+            </div>
 
-      </Box>
-        <Grid container sx={{ mx: 'auto', width: 100 }}>
-          <Button type="submit" variant="contained" sx={{ mt: 3, mb: 3 }}>
-            Create
-          </Button>
-        </Grid>
-      </Card>}
+            <CardHeader subheader="Employee Address" />
 
+            <TextField id="neighbourhood" name="neighbourhood" label="Neighbourhood" variant="filled" />
+            <TextField id="district" name="district" label="District" variant="filled" />
+            <TextField id="province" name="province" label="Province" variant="filled" />
+            <TextField id="country" name="country" label="Country" variant="filled" />
+            <TextField id="buildingNumber" name="buildingNumber" label="Building Number" variant="filled" />
+            <TextField id="apartmentNumber" name="apartmentNumber" label="Apartment Number" variant="filled" />
+            <TextField id="postalCode" name="postalCode" label="Postal Code" variant="filled" />
 
+            <Grid container sx={{ mx: 'auto', width: 100 }}>
+              <Button type="submit" variant="contained" sx={{ mt: 3, mb: 3 }}>
+                Create
+              </Button>
+            </Grid>
+          </Box>
+        </Card>
+      )}
     </Card>
   );
-
 }
