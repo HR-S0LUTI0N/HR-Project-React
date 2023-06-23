ae3b176ce3ab3a467118a9b6fb0e7fda5162d219
TCenkErguven <tunccenkerguven@gmail.com>
2023-06-23 10:01:34 +0300

    Title kaldırıldı

--- src/pages/registerManager.js
+++ src/pages/registerManager.js
@@ -65,7 +65,7 @@ export default function SignInSide() {
     }
   };
   const handleNameChange = (event) => {
-    const name = event.target.value.trim().charAt(0).toUpperCase() +event.target.value.trim().slice(1).toLowerCase()
+    const name = event.target.value.trim().charAt(0).toUpperCase() + event.target.value.trim().slice(1).toLowerCase()
     setName(name);
 
     const validateName = () => {
@@ -88,7 +88,7 @@ export default function SignInSide() {
   };
 
   const handleMiddleChange = (event) => {
-    const middleName = event.target.value.trim().charAt(0).toUpperCase() +event.target.value.trim().slice(1).toLowerCase()
+    const middleName = event.target.value.trim().charAt(0).toUpperCase() + event.target.value.trim().slice(1).toLowerCase()
     setMiddlename(middleName);
 
     const validateMiddleName = () => {
@@ -108,7 +108,7 @@ export default function SignInSide() {
   };
 
   const handleSurnameChange = (event) => {
-    const surname = event.target.value.trim().charAt(0).toUpperCase() +event.target.value.trim().slice(1).toLowerCase()
+    const surname = event.target.value.trim().charAt(0).toUpperCase() + event.target.value.trim().slice(1).toLowerCase()
     setSurname(surname);
     const validateSurname = () => {
       if (surname.trim().length === 0) {
@@ -217,7 +217,7 @@ export default function SignInSide() {
   };
 
   const handleDepartmentChange = (event) => {
-    const department = event.target.value.trim().charAt(0).toUpperCase() +event.target.value.trim().slice(1).toLowerCase()
+    const department = event.target.value.trim().charAt(0).toUpperCase() + event.target.value.trim().slice(1).toLowerCase()
     setDepartment(department);
 
     const validateDepartment = () => {
@@ -533,7 +533,7 @@ export default function SignInSide() {
                 <Select labelId="company-label" id="company" value={company} onChange={handleCompanyChange}>
                   {companies.map((company) => (
                     <MenuItem key={company.companyId} value={company}>
-                      {`${company.companyName} ${company.title}`}
+                      {`${company.companyName} `}
                     </MenuItem>
                   ))}
                 </Select>
